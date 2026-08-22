e58c4be4f145191484c7dbfbc37552a2c30f713f
H80944 <ygy438735@gmail.com>
2026-08-22 16:56:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+app.use(express.static('public'));
 const express = require('express');
 const morgan = require('morgan');
-const controller = require("./controller")
+const controller = require("./controller");
 
 const app = express();
 
@@ -8,13 +9,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.static("./public"));
 
 app.use((req, res, next) => {
-    req.body.timestamp = new Date().toISOString();
-    next();
+  req.body.timestamp = new Date().toISOString();
+  next();
 });
 
 const router = express.Router();
@@ -23,5 +24,10 @@ router
   .post(controller.postCredentials);
 
 app.use('/api/v1', router);
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 module.exports = app;
